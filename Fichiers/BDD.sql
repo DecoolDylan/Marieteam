c1100959e7c0650e88a43733cb6d402bba3e1c18
Decool Dylan <dylan.decool14@gmail.com>
2021-06-15 14:31:48 +0200

    Finition

--- Fichiers/BDD.sql
+++ Fichiers/BDD.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 31 mai 2021 à 11:07
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `marieteam`
 --
+CREATE DATABASE IF NOT EXISTS `marieteam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `marieteam`;
 
 -- --------------------------------------------------------
 
@@ -37,17 +21,7 @@ CREATE TABLE IF NOT EXISTS `bateau` (
   `vitesse` float NOT NULL,
   `equipements` varchar(80) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`id_bateau`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `bateau`
---
-
-INSERT INTO `bateau` (`id_bateau`, `nom`, `longueur`, `largeur`, `image`, `vitesse`, `equipements`) VALUES
-(1, 'Kor\' Ant ', 1800, 30, 'http://www.semainedunautisme.com/images/bateau-gonflable-rigide_10.jpg', 30, 'Accès Handicapé.Bar.Pont promenade.Plongée.'),
-(2, 'Ar Solen', 550, 30, 'https://www.masculin.com/images/article/9946/5-bateaux-de-luxe-qui-envoient-du-reve.jpg', 50, 'Accès Handicapé.Bar.Pont promenade.Plongée.'),
-(3, 'Al\'xi', 850, 50, 'https://www.masculin.com/images/article/9946/5-bateaux-de-luxe-qui-envoient-du-reve.jpg', 50, 'Accès Handicapé.Bar.Pont promenade.Baby foot.Dansoir.'),
-(4, 'Luce isle', 1100, 50, 'https://www.seafaridiving.fr/medias/img/750x420/captain-sparrow-bateau-seafari-plongee.jpg', 80, 'Accès Handicapé.Bar.Pont promenade.Baby foot.Dansoir.');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,15 +35,6 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `libelle` varchar(80) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`lettre_categorie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `categorie`
---
-
-INSERT INTO `categorie` (`lettre_categorie`, `libelle`) VALUES
-('A', 'Passager'),
-('B', 'Veh.inf.2m'),
-('C', 'Veh.sup.2m');
 
 -- --------------------------------------------------------
 
@@ -85,24 +50,6 @@ CREATE TABLE IF NOT EXISTS `contenir` (
   PRIMARY KEY (`lettre_categorie`,`id_bateau`),
   KEY `CONTENIR_BATEAU0_FK` (`id_bateau`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `contenir`
---
-
-INSERT INTO `contenir` (`lettre_categorie`, `id_bateau`, `capaciteMax`) VALUES
-('A', 1, 240),
-('A', 2, 150),
-('A', 3, 100),
-('A', 4, 500),
-('B', 1, 20),
-('B', 2, 30),
-('B', 3, 30),
-('B', 4, 50),
-('C', 1, 5),
-('C', 2, 10),
-('C', 3, 20),
-('C', 4, 25);
 
 -- --------------------------------------------------------
 
@@ -124,13 +71,6 @@ CREATE TABLE IF NOT EXISTS `liaison` (
   KEY `LIAISON_PORT1_FK` (`port_arrivee`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `liaison`
---
-
-INSERT INTO `liaison` (`code_liaison`, `nom`, `distance_miles`, `id_secteur`, `port_depart`, `port_arrivee`) VALUES
-(1, 'Le Palais - Quiberon', 55, 1, 2, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -144,15 +84,6 @@ CREATE TABLE IF NOT EXISTS `periode` (
   PRIMARY KEY (`dateDeb`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `periode`
---
-
-INSERT INTO `periode` (`dateDeb`, `dateFin`) VALUES
-('2020-09-01', '2021-06-15'),
-('2021-06-16', '2021-09-15'),
-('2021-09-16', '2022-05-31');
-
 -- --------------------------------------------------------
 
 --
@@ -164,20 +95,7 @@ CREATE TABLE IF NOT EXISTS `port` (
   `id_port` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(80) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`id_port`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `port`
---
-
-INSERT INTO `port` (`id_port`, `nom`) VALUES
-(1, 'Quiberon'),
-(2, 'Le Palais'),
-(3, 'Sauzon'),
-(4, 'Vannes'),
-(5, 'Port St Gildas'),
-(6, 'Port-Tudy'),
-(7, 'Lorient');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -217,23 +135,7 @@ CREATE TABLE IF NOT EXISTS `secteur` (
   `id_secteur` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(80) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`id_secteur`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `secteur`
---
-
-INSERT INTO `secteur` (`id_secteur`, `nom`) VALUES
-(1, 'Belle-Ils-en-mer'),
-(2, 'Houat'),
-(3, 'Ils de Groix'),
-(4, 'Ouessant'),
-(5, 'Molene'),
-(6, 'Sein'),
-(7, 'Brehat'),
-(8, 'Batz'),
-(9, 'Aix'),
-(10, 'Yeu');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -285,20 +187,6 @@ CREATE TABLE IF NOT EXISTS `type` (
   KEY `TYPE_CATEGORIE_FK` (`lettre_categorie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `type`
---
-
-INSERT INTO `type` (`num_type`, `libelle`, `lettre_categorie`) VALUES
-(1, 'Adulte', 'A'),
-(2, 'Junior 8-18 ans', 'A'),
-(3, 'Enfant 0-7 ans', 'A'),
-(4, 'Voiture long.inf.4m', 'B'),
-(5, 'Voiture long.inf.5m', 'B'),
-(6, 'Fourgon', 'C'),
-(7, 'Camping Car', 'C'),
-(8, 'Camion', 'C');
-
 -- --------------------------------------------------------
 
 --
@@ -318,16 +206,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `point_fidelite` int(11) NOT NULL,
   `grade` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `mail`, `mot_de_passe`, `adresse`, `code_postal`, `ville`, `point_fidelite`, `grade`) VALUES
-(1, 'Decool', 'Dylan', 'dylan.decool14@gmail.com', '$2y$10$RjTyEYwcXsGVGnWKKS/zQuOUNDjSz9mXCq88szRyNVxFUzc5qrO.O', '24 rue des ormeaux', 59940, 'Estaires', 1000, 1),
-(2, 'Legrand', 'Baptiste', 'legrand.baptiste@gmail.com', '$2y$10$RjTyEYwcXsGVGnWKKS/zQuOUNDjSz9mXCq88szRyNVxFUzc5qrO.O', '12 rue des chenilles', 59000, 'Lille', 0, 0),
-(3, 'Fontaine', 'Jean', 'fontaine.jean@gmail.com', '$2y$10$RjTyEYwcXsGVGnWKKS/zQuOUNDjSz9mXCq88szRyNVxFUzc5qrO.O', '560 rue des chansons', 59190, 'Hazebrouck', 125, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contraintes pour les tables déchargées
@@ -377,6 +256,3 @@ ALTER TABLE `type`
   ADD CONSTRAINT `TYPE_CATEGORIE_FK` FOREIGN KEY (`lettre_categorie`) REFERENCES `categorie` (`lettre_categorie`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
